7977261ae01a22e943384433bfee07d3a21d48b5
slanzavecchia <slanzavecchia@alumnos.exa.unicen.edu.ar>
2024-05-30 21:25:38 -0300

    Merge dudoso, a ver Gaston

--- WebSeries/src/app/components/header/header.component.ts
+++ WebSeries/src/app/components/header/header.component.ts
@@ -1,12 +1,39 @@
 import { Component } from '@angular/core';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { MenuModule } from 'primeng/menu';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ToolbarModule, ButtonModule, MenuModule, ToastModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  items: any[];
 
+  constructor() {
+    this.items = [
+      {
+        label: 'File',
+        items: [
+          { label: 'New', icon: 'pi pi-fw pi-plus' },
+          { label: 'Open', icon: 'pi pi-fw pi-external-link' }
+        ]
+      },
+      {
+        label: 'Edit',
+        items: [
+          { label: 'Undo', icon: 'pi pi-fw pi-undo' },
+          { label: 'Redo', icon: 'pi pi-fw pi-redo' }
+        ]
+      }
+    ];
+  }
+
+  toggleMenu(event: Event, menu: any) {
+    menu.toggle(event);
+  }
 }
--- WebSeries/src/app/screens/main/main.component.ts
+++ WebSeries/src/app/screens/main/main.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CarouselComponent } from '../../components/carousel/carousel.component';
+import { HeaderComponent } from '../../components/header/header.component';
 
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CarouselComponent],
+  imports: [CarouselComponent, HeaderComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
